f975854d1af71a8db6a79744e63db1d8cd759b41
abhishek kumar <flashtoak@gmail.com>
2023-06-04 00:18:22 +0530

    improved ui dark-mode light-mode

--- src/script.js
+++ src/script.js
@@ -1,6 +1,15 @@
 function toggleDarkMode() {
-    const body = document.body;
-    body.classList.toggle("dark-mode");
+  const body = document.body;
+  body.classList.toggle("body-dark-mode");
+  document.querySelectorAll("*").forEach((El) => {
+    const elements = ["P", "H3", "H1", "INPUT", "LABEL"];
+    if (elements.includes(El.nodeName)) {
+      El.classList.toggle("dark");
+    }
+  });
+  const btnEl = document.querySelector("button");
+  btnEl.classList.toggle("dark-btn");
+  document.querySelector(".dark-btn")
+    ? (btnEl.textContent = "SWITCH TO LIGHT MODE")
+    : (btnEl.textContent = "SWITCH TO DARK MODE");
 }
-
-
